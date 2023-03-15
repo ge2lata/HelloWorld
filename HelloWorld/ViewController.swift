dd28f87c151b82e7ffdf9f929db230b92b5d33e5
ge2lata <127960248+ge2lata@users.noreply.github.com>
2023-03-15 20:21:11 +0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Оксана Спичёнок on 15.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
